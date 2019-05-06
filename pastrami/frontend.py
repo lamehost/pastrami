b42abd039fc74be3b610702d7ed8aaaf54480fd6
Marco Marzetti <marco@lamehost.it>
2019-05-06 20:03:28 +0200

    post text from cisco router supported

--- pastrami/frontend.py
+++ pastrami/frontend.py
@@ -2,7 +2,7 @@
 
 from __future__ import absolute_import
 
-from flask import Flask, render_template, current_app, abort
+from flask import Flask, render_template, current_app, abort, request
 
 
 def create_app(config=None):
@@ -12,13 +12,29 @@ def create_app(config=None):
         for key, value in config.items():
             application.config[key] = value
 
-    @application.route('/')
-    @application.route('/<string:text_id>')
+    try:
+        application.debug = config['debug']
+    except KeyError:
+        pass
+
+    @application.route('/', methods=['GET'])
+    @application.route('/<string:text_id>', methods=['GET'])
     def index_html(text_id=False):
         text = {}
         if text_id:
             text = get_content_by_id(text_id)
         return render_template('index.html', text=text)
+
+    @application.route('/<string:text_id>', methods=['PUT'])
+    def put_text(text_id=False):
+        '''Create a new text'''
+        database = current_app.config['__db_instance']
+
+        body = request.data.decode("utf-8")
+        text = database.text(text=body, text_id=text_id)
+        database.session.add(text)
+        database.session.commit()
+        return "OK", 200
 
     @application.route('/<string:text_id>/text')
     def text_html(text_id):
