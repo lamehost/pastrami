43c82f4d4595b4e3e2ef9c3de14a5d013d78961c
Marco Marzetti <marco@lamehost.it>
2019-08-31 22:23:04 +0200

    first commit

---
+++ pastrami/__about__.py
@@ -0,0 +1,16 @@
+__version__ = "0.3.0"
+__author__ = "Marco Marzetti"
+__author_email__ = "marco@lamehost.it"
+__url__ = "https://github.com/lamehost/anastasia/"
+__description__ = "**VERY** minimalistic REST interface for image upload"
+__license__ = "MIT"
+# __classifiers__ = [
+#     'Intended Audience :: Developers',
+#     'Topic :: Software Development :: Libraries :: Python Modules',
+#     'Topic :: System :: Networking',
+#     'License :: OSI Approved :: MIT License',
+#     'Programming Language :: Python :: 2.7',
+#     'Programming Language :: Python :: 3.6'
+# ]
+__classifiers__ = []
+
