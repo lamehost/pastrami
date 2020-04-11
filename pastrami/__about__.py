782cc0eb0a907674e4ab5b23197c4e38fe620ae9
Marco Marzetti <marco@lamehost.it>
2020-04-11 14:29:58 +0200

    Version bump

--- pastrami/__about__.py
+++ pastrami/__about__.py
@@ -20,18 +20,17 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE
 
-__version__ = "0.4.1"
+__version__ = "0.3.0"
 __author__ = "Marco Marzetti"
 __author_email__ = "marco@lamehost.it"
-__url__ = "https://github.com/lamehost/pastrami/"
-__description__ = "Pastebin service"
+__url__ = "https://github.com/lamehost/anastasia/"
+__description__ = "**VERY** minimalistic REST interface for image upload"
 __license__ = "MIT"
- __classifiers__ = [
-     'Intended Audience :: Developers',
-     'Topic :: Software Development :: Libraries :: Python Modules',
-     'Topic :: System :: Pastebin',
-     'License :: OSI Approved :: MIT License',
-     'Programming Language :: Python :: 3.6'
- ]
+__classifiers__ = [
+   'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6'
+]
 __classifiers__ = []
 
