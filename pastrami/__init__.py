979920817819b40b2327e2560222e8c6d1687831
Marco Marzetti <marco@lamehost.it>
2022-12-30 22:58:31 +0100

    Code cleanup

--- pastrami/__init__.py
+++ pastrami/__init__.py
@@ -22,6 +22,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE
 
+"""
+Pastebin web service.
+"""
+
 from .webapp import create_app
 from .database import TextSchema
 
--- pastrami/__main__.py
+++ pastrami/__main__.py
@@ -22,6 +22,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE
 
+"""
+Main entrypoint for the package
+"""
 
 import os
 import uvicorn
