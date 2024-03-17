ee93f6f5d7c904061c0d10a71c1382d815eda369
Marco Marzetti <mmarzetti@pccwglobal.com>
2024-03-17 16:32:18 +0100

    code cleanup

--- tests/test_webapp.py
+++ tests/test_webapp.py
@@ -9,11 +9,11 @@ class TestWebApp(unittest.TestCase):
     def setUp(self):
         self.settings = {
             # Database
-            "database":  {
+            "database": {
                 "url": "sqlite:///:memory:",
                 "create": True,
                 "echo": False,
-                "encrypted": True
+                "encrypted": True,
             },
             # API frontend stuff
             "contact": {
@@ -25,58 +25,47 @@ class TestWebApp(unittest.TestCase):
             "docs": False,
             # Text
             "dayspan": 1,
-            "maxlength": 10
+            "maxlength": 10,
         }
 
         self.app = create_app(settings=self.settings)
 
-#    def tearDown(self):
-#        os.unlink('./test.db')
+    def test_frontend_methods(self):
+        with TestClient(self.app) as client:
+            response = client.get("/")
 
-    def test_http_methods(self):
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("<title>Pastrami</title>", response.text)
+
+    def test_api_methods(self):
         with TestClient(self.app) as client:
             # Create text
-            response = client.post(
-                "/",
-                json={"content": "FooBar"}
-            )
+            response = client.post("/", json={"content": "FooBar"})
             text = response.json()
 
             self.assertEqual(response.status_code, 200)
-            self.assertEqual(text['content'], "FooBar")
+            self.assertEqual(text["content"], "FooBar")
 
             # Create invalid text
-            response = client.post(
-                "/",
-                json={"content": "Very very long message"}
-            )
+            response = client.post("/", json={"content": "Very very long message"})
             self.assertEqual(response.status_code, 406)
 
             # Get existing
-            response = client.get(
-                f"/{text['text_id']}"
-            )
-            self.assertIn(
-                '<pre class="prettyprint linenums:1">FooBar</pre>',
-                response.text
-            )
+            response = client.get(f"/{text['text_id']}")
+            self.assertIn('<pre class="prettyprint linenums:1">FooBar</pre>', response.text)
 
             # Get non-existing
-            response = client.get('f/WRONG')
+            response = client.get("f/WRONG")
             self.assertEqual(response.status_code, 404)
 
             # Delete existing
-            response = client.delete(
-                f"/{text['text_id']}"
-            )
+            response = client.delete(f"/{text['text_id']}")
             self.assertEqual(response.status_code, 204)
 
             # Delete non existing
-            response = client.delete(
-                f"/{text['text_id']}"
-            )
+            response = client.delete(f"/{text['text_id']}")
             self.assertEqual(response.status_code, 404)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main(verbosity=2)
